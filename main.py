1ee3230961b2b2ee010e25a800b37580c6bbe93b
Chaitra Patil <85379658+chaitra751@users.noreply.github.com>
2026-09-07 22:06:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,3 +84,10 @@ from langchain_core.output_parsers import StrOutputParser
 def format_docs(retrieved_docs):
   context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
   return context_text
+
+parallel_chain = RunnableParallel({
+    'context': retriever | RunnableLambda(format_docs),
+    'question': RunnablePassthrough()
+})
+
+parser = StrOutputParser()
