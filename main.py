872cf65eeb09a63d542e2549497904ad418570fd
Chaitra Patil <85379658+chaitra751@users.noreply.github.com>
2026-09-07 20:32:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import streamlit as st
 from pathlib import Path
 import base64
 import streamlit.components.v1 as components
-
-
-
-
 import faiss
+
+
+import streamlit as st
+
+st.title("VM0042 Agent")
+
+st.write("Hi! Welcome to the VM0042 Question Answering System.")
 
 # 1. Load the FAISS index
 index = faiss.read_index("index.faiss")
