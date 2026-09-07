ffc97140ff7a9b7986f2e712e15720d58c238a6b
Chaitra Patil <85379658+chaitra751@users.noreply.github.com>
2026-09-07 21:29:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,9 @@ st.write(vector_store )
 st.write("Number of vectors:", vector_store .ntotal)
 st.write("Vector dimension:", vector_store .d)
 
-
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
 
 vector_store = FAISS.load_local(
     "vector_store",
