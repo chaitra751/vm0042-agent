e25c4a7d32427909d82b3a77602096f5250c2d3a
Chaitra Patil <85379658+chaitra751@users.noreply.github.com>
2026-09-07 22:05:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -77,3 +77,10 @@ prompt = PromptTemplate(
 
 question = "'What is vm0042"
 retrieved_docs = retriever.invoke(question)
+
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
+
+def format_docs(retrieved_docs):
+  context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+  return context_text
