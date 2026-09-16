35f1f48ecd5fac5a5c4d4a21085dea76b61212f6
Chaitra Patil <85379658+chaitra751@users.noreply.github.com>
2026-09-16 14:46:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ retriever = vector_store.as_retriever(
 def load_llm():
 
     llm = HuggingFaceEndpoint(
-        repo_id="meta-llama/Llama-3.1-8B-Instruct",
+        repo_id="meta-llama/Llama-3.3-70B-Instruct-evals",
         task="text-generation",
         max_new_tokens=512,
         temperature=0.1,
